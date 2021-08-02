b215bdf9db97fc09699260335d9a81d24f6ff008
Dan Seminara <japaneseforgaming@gmail.com>
2021-08-02 14:43:10 -0400

    Assignment complete!

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+
+function writeCards(people, occasion) {
+    
+    let thankYous = [];
+
+    for (let i=0; i < people.length; i++) {
+        thankYous.push(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`);
+    }
+
+    return thankYous;
+
+}
+
+
+function countDown(howLong) {
+    while (howLong > -1) {
+        console.log(howLong);
+        howLong --;
+    }
+}
+
